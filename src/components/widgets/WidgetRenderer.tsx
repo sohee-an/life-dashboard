bfa80b9571bed0c0a7513caf961c76b7e5f1e383
sohee-an <soan32@hanmail.net>
2025-10-10 15:19:34 +0900

    fix: widget 타입 Discriminated Union으로수정

--- src/components/widgets/WidgetRenderer.tsx
+++ src/components/widgets/WidgetRenderer.tsx
@@ -1,19 +1,11 @@
-import React from "react";
 import TextWidget from "./TextWidget";
 import type { Widget } from "../../types/widget";
 
-export default function WidgetRenderer<T extends Widget["type"]>({
-  widget,
-}: {
-  widget: Widget<T>;
-}) {
+export default function WidgetRenderer({ widget }: { widget: Widget }) {
   switch (widget.type) {
-    // case "text":
-    //   return <TextWidget id={widget.id} content={widget.props.content} />;
-    case "text": {
-      const w = widget as Widget<"text">;
-      return <TextWidget id={w.id} content={w.props.content} />;
-    }
+    case "text":
+      return <TextWidget id={widget.id} content={widget.props.content} />;
+
     default:
       return <div>❓ Unknown widget type</div>;
   }
--- src/types/widget.ts
+++ src/types/widget.ts
@@ -1,21 +1,48 @@
-export type WidgetType = "text" | "budget" | "chart";
+// export type WidgetType = "text" | "budget" | "chart";
 
-export interface WidgetPropsMap {
-  text: { content: string };
-  budget: { amount: number; category: string };
-  chart: { data: number[]; color: string };
-}
+// export interface WidgetPropsMap {
+//   text: { content: string };
+//   budget: { amount: number; category: string };
+//   chart: { data: number[]; color: string };
+// }
 
-// Widget 제네릭화
-export interface Widget<T extends WidgetType = WidgetType> {
-  id: string;
-  type: T;
-  layout: {
-    i: string;
-    x: number;
-    y: number;
-    w: number;
-    h: number;
-  };
-  props: WidgetPropsMap[T];
-}
+// // Widget 제네릭화
+// export interface Widget<T extends WidgetType = WidgetType> {
+//   id: string;
+//   type: T;
+//   layout: {
+//     i: string;
+//     x: number;
+//     y: number;
+//     w: number;
+//     h: number;
+//   };
+//   props: WidgetPropsMap[T];
+// }
+export type Layout = {
+  i: string;
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+};
+
+export type Widget =
+  | {
+      id: string;
+      type: "text";
+      layout: Layout;
+      props: { content: string };
+    }
+  | {
+      id: string;
+      type: "budget";
+      layout: Layout;
+      props: { amount: number; category: string };
+    }
+  | {
+      id: string;
+      type: "chart";
+      layout: Layout;
+      props: { data: number[]; color: string };
+    };

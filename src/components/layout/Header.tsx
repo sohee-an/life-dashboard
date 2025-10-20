4e31fee18af329065234739b030f754f4449d9cb
sohee-an <soan32@hanmail.net>
2025-10-20 15:11:19 +0900

    fix: nvmrc 추가 , header css 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="fixed top-0 left-0 h-20 bg-gray-200 text-black flex items-center w-full">
+    <header className="fixed top-0 left-0 h-14 bg-gray-200 text-black flex items-center w-full">
       <h1 className="text-3xl mb-2 text-customRed">📊 Life Dashboard</h1>
     </header>
   );
